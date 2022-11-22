78a9b4acd049bf434958fac855493df064d3f07a
carlos <carcanbla.alu@iesbenigaslo.es>
2022-11-22 13:52:35 +0100

    22/11/2022

--- app/src/main/java/com/example/incivismenavigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/incivismenavigation/ui/home/HomeFragment.java
@@ -2,14 +2,22 @@ package com.example.incivismenavigation.ui.home;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -18,10 +26,27 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.incivismenavigation.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    ActivityResultLauncher<String[]> locationPermissionRequest;
+    private Location mLastLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private boolean mTrackingLocation;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,9 +56,18 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        binding.btnGetLocation.setOnClickListener((View clickedView) -> {
+            if (!mTrackingLocation) {
+                startTrackingLocation();
+            } else {
+                stopTrackingLocation();
+            }
+        });
 
         locationPermissionRequest = registerForActivityResult(new ActivityResultContracts
                         .RequestMultiplePermissions(), result -> {
@@ -51,6 +85,58 @@ public class HomeFragment extends Fragment {
                 }
         );
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+    mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult != null) {
+                fetchAddress(locationResult.getLastLocation());
+            } else {
+                binding.textHome.setText("Sense localització coneguda");
+            }
+        }
+    };
+
+     binding.btnGetLocation.setOnClickListener(view -> {
+        Toast.makeText(requireContext(), "Clicked Get Location", Toast.LENGTH_SHORT).show();
+        if (!mTrackingLocation) {
+            startTrackingLocation();
+        } else {
+            stopTrackingLocation();
+        }
+    });
+
+        return root;
+}
+    private void startTrackingLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show();
+            locationPermissionRequest.launch(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+        } else {
+            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+            mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+        }
+        binding.textHome.setText("Carregant...");
+        binding.loading.setVisibility(ProgressBar.VISIBLE);
+        mTrackingLocation = true;
+        binding.btnGetLocation.setText("Aturar el seguiment de la ubicació");
+    }
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            binding.loading.setVisibility(ProgressBar.INVISIBLE);
+            mTrackingLocation = false;
+            binding.btnGetLocation.setText("Comença a seguir la ubicació");
+        }
     }
 
     private final String TAG = this.getClass().getSimpleName();
@@ -67,7 +153,73 @@ public class HomeFragment extends Fragment {
             });
         } else {
             Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    location -> {
+                        if (location != null) {
+                            fetchAddress(location);
+                        } else {
+                            binding.textHome.setText("Sense localització coneguda");
+                        }
+                    });
         }
+        binding.textHome.setText("Carregant...");
+    }
+
+
+    private void fetchAddress(Location location) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        Geocoder geocoder = new Geocoder(requireContext(),
+                Locale.getDefault());
+
+        executor.execute(() -> {
+            // Aquest codi s'executa en segon pla
+            List<Address> addresses = null;
+            String resultMessage = "";
+
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // En aquest cas, sols volem una única adreça:
+                        1);
+
+
+                if (addresses == null || addresses.size() == 0) {
+                    if (resultMessage.isEmpty()) {
+                        resultMessage = "No s'ha trobat cap adreça";
+                        Log.e("INCIVISME", resultMessage);
+                    }
+                } else {
+                    Address address = addresses.get(0);
+                    ArrayList<String> addressParts = new ArrayList<>();
+
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressParts.add(address.getAddressLine(i));
+                    }
+
+                    resultMessage = TextUtils.join("\n", addressParts);
+                    String finalResultMessage = resultMessage;
+                    handler.post(() -> {
+                        // Aquest codi s'executa en primer pla.
+                        binding.textHome.setText(String.format(
+                                "Direcció: %1$s \n Hora: %2$tr",
+                                finalResultMessage, System.currentTimeMillis()));
+                    });
+                }
+
+            } catch (IOException ioException) {
+                resultMessage = "Servei no disponible";
+                Log.e("INCIVISME", resultMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                resultMessage = "Coordenades no vàlides";
+                Log.e("INCIVISME", resultMessage + ". " +
+                        "Latitude = " + location.getLatitude() +
+                        ", Longitude = " +
+                        location.getLongitude(), illegalArgumentException);
+            }
+        });
     }
 
     @Override
